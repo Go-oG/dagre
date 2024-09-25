787f4e52de364724b0a541dada36e825d0847c81
ggg <>
2024-09-25 10:50:53 +0800

    更新order/barycenter

--- lib/src/order/barycenter.dart
+++ lib/src/order/barycenter.dart
@@ -1,4 +1,3 @@
-import 'package:dart_dagre/src/model/edge.dart';
 import 'package:dart_dagre/src/model/edge_props.dart';
 import 'package:dart_dagre/src/model/node_props.dart';
 
@@ -6,26 +5,26 @@ import '../graph/graph.dart';
 import '../model/tmp/order_inner_result.dart';
 
 List<OrderInnerResult> barycenter(Graph g, List<String> movable) {
-  List<OrderInnerResult> r= List.from(movable.map((v) {
+return List.from(movable.map((v) {
     List<EdgeObj> inV = g.inEdges(v);
     if (inV.isEmpty) {
       return OrderInnerResult(v);
     } else {
-      Map<String, num> initP = {'sum': 0, 'weight': 0};
-      Map<String, num> result = initP;
+      Map<String, num> acc = {'sum': 0, 'weight': 0};
       for (int i = 0; i < inV.length; i++) {
         var tt = inV[i];
         EdgeProps edge = g.edge(tt.v, tt.w, tt.id);
         NodeProps nodeU = g.node(inV[i].v);
-        num sum = result['sum']! + (edge.weight * nodeU.order);
-        num weight = result['weight']! + edge.weight;
-        result = {'sum': sum, 'weight': weight};
+
+        num sum = acc['sum']! + (edge.weight * nodeU.order!);
+        num weight = acc['weight']! + edge.weight;
+        acc = {'sum': sum, 'weight': weight};
       }
+
       OrderInnerResult p = OrderInnerResult(v);
-      p.barycenter = result['sum']! / result['weight']!;
-      p.weight = result['weight']!;
+      p.barycenter = acc['sum']! / acc['weight']!;
+      p.weight = acc['weight']!;
       return p;
     }
   }));
-  return r;
 }
